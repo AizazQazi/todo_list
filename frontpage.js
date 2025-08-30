330e445e94481683f64e957783982e9408a4014c
AizazQazi <aizazfasseh@gmail.com>
2025-08-30 11:10:07 +0500

    crud implementd

--- frontpage.js
+++ frontpage.js
@@ -1,17 +1,86 @@
+const button= document.getElementById("add")
+const form = document.getElementById("taskform")
+const tasklist=document.getElementById("tasklist")
+
+form.style.display="none"
     
-    const button= document.getElementById("add")
-    const originaltext=button.innerText
-    
-    console.log(originaltext)
-    
-    
-    button.addEventListener("click", function(){
-        if (button.innerText===originaltext) {
-        button.innerText="Hi...."
-    }
-    else{
+button.addEventListener("click", function(){
+     if (form.style.display==="none" || form.style.display==="") {
+        form.style.display="block"
+        button.innerText="hide form"
+     } 
+     else {
+        form.style.display="none"
         button.innerText="Add new task"
-    }
+        
+     }
 
 })
 
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const task= document.getElementById("task").value;
+    
+    console.log("new task::", task);
+    
+    const li=document.createElement("li")
+    tasklist.appendChild(li)
+    form.reset();
+
+    const editbtn=document.createElement("button")
+    editbtn.textContent="Edit"
+
+    const span=document.createElement("span")
+    span.textContent=task
+    li.appendChild(span)
+
+    editbtn.addEventListener("click", function(){
+
+
+       if (form.style.display==="block") {
+        form.style.display="none"
+        button.innerText="Add new task"
+       }
+        
+        if (editbtn.textContent==="Edit") {
+            const input=document.createElement("input")
+            input.type="text"
+            input.value=span.textContent
+ 
+            li.replaceChild(input,span)
+         
+            editbtn.textContent="save"
+        }
+
+        else {
+             const input=li.querySelector("input")
+             span.textContent=input.value
+ 
+             li.replaceChild(span,input)
+ 
+             editbtn.textContent="Edit"
+             
+        }
+
+    
+    
+})
+
+    // del button
+
+    const delbtn=document.createElement("button")
+    delbtn.textContent="delete"
+
+    delbtn.addEventListener("click", function(){
+
+        li.remove()
+
+    })
+
+    li.appendChild(editbtn)  
+    li.appendChild(delbtn)
+    
+
+})
